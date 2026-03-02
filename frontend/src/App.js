dab9c47e863418d884fbf314c33dbe405e8d3c8c
fenilvanpariya81-cpu <fenilvanpariya81@gmail.com>
2026-03-02 15:57:01 +0530

    ghdf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ import { ViewBlog } from './Components/ViewBlog/ViewBlog';
 import { MostLiked } from './Components/LikesAndComments/MostLiked';
 import { MostCommented } from './Components/LikesAndComments/MostCommented';
 import SignIn from './Components/Authentication/SignIn';
-import SignUp from './Components/Authentication/SignUp';
+import SignUp from './Components/Authenticavzvczxvtion/SignUpczxgxgvbv';
 import { UnauthorizeUser } from './Components/Authentication/UnauthorizeUser';
 
 
